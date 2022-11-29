80579af9c95b7f0f026c0e06fa12c2b07a00e31a
Jan Claeys <jan.claeys@tillit.be>
2022-11-29 13:41:31 +0100

    Get data from database

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -23,24 +23,15 @@ async function download() {
   await Excel.run(async (context) => {
     const res = await axios.get('http://localhost:4000');
     const data = res.data;
-    const keys = Object.keys(data);
-    console.log(keys);
+    const keys = Object.keys(data[0]);
 
     const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-    const expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
+    console.log(keys.length);
+    const expensesTable = currentWorksheet.tables.add("A1:F1", true /*hasHeaders*/);
     expensesTable.name = "ExpensesTable";
 
-    expensesTable.getHeaderRowRange().values = [["Date", "Merchant", "Category", "Amount"]];
-
-    expensesTable.rows.add(null /*add at the end*/, [
-      ["1/1/2017", "The Phone Company", "Communications", "120"],
-      ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
-      ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
-      ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
-      ["1/11/2017", "Bellows College", "Education", "350.1"],
-      ["1/15/2017", "Trey Research", "Other", "135"],
-      ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"],
-    ]);
+    expensesTable.getHeaderRowRange().values = [keys];
+    expensesTable.rows.add(null /*add at the end*/, data.map(Object.values));
 
     expensesTable.columns.getItemAt(3).getRange().numberFormat = [["\u20AC#,##0.00"]];
     expensesTable.getRange().format.autofitColumns();
